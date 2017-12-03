9f149bfa03ea3728b73e07a60d8b03559957cf91
Stefan Grassberger <stefan.grassberger@gmail.com>
2017-12-03 10:33:46 +0100

    GameManager script and temp sprites

---
+++ Home Invasion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public bool doingSetup;
+
+	private void Awake() {
+		if( instance == null )
+			instance = this;
+		else if( instance != this )
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+	void InitGame() {
+		doingSetup = true;
+
+		// ...
+
+		doingSetup = false;
+	}
+
+	void Update () {
+		if( doingSetup )
+			return;
+
+		// ...
+	}
+}
