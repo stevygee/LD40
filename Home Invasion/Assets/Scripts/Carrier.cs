95381e3b61068decb6f109ce29217514ca5bbe56
Stefan Grassberger <stefan.grassberger@gmail.com>
2017-12-03 15:33:37 +0100

    Thief AI state machine

--- Home Invasion/Assets/Scripts/Carrier.cs
+++ Home Invasion/Assets/Scripts/Carrier.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Carrier : MonoBehaviour {
 
 	private GameObject item;
+	private bool pickingUp = false;
 
 	public float reach;
+	public bool hasItem;
 
 	public void PickUp(GameObject newItem) {
-		if( !item ) {
+		if( !hasItem && !pickingUp ) {
 			item = newItem;
-			print("Picked up item!");
+			// TODO: Timer
+
+			item.transform.SetParent(gameObject.transform);
+
+			pickingUp = true;
+			hasItem = true;
 		}
 	}
 
 	public void Drop() {
 		item = null;
+	}
+
+	public void Update() {
+		if( hasItem ) {
+			item.transform.localPosition = new Vector3(0, 1.5f, 0);
+			item.transform.localRotation = Quaternion.identity;
+		}
 	}
 }
---
+++ Home Invasion/Assets/Scripts/ThiefAI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefAI : MonoBehaviour {
+
+	private Carrier carrier;
+	private GameManager gameMgr;
+	private Rigidbody2D rb;
+	private Animator aiState;
+	private Vector3 relativeItemPos;
+
+	private int searchingStateHash = Animator.StringToHash("Base Layer.Searching");
+	private int pickingUpItemStateHash = Animator.StringToHash("Base Layer.PickingUpItem");
+	private int leavingStateHash = Animator.StringToHash("Base Layer.Leaving");
+	private int fleeingStateHash = Animator.StringToHash("Base Layer.Fleeing");
+
+	private int itemInRangeHash = Animator.StringToHash("itemInRange");
+	private int scaredHash = Animator.StringToHash("scared");
+	private int hasItemHash = Animator.StringToHash("hasItem");
+
+	public bool active;
+	public float speed;
+
+	void Start() {
+		gameMgr = GameManager.instance;
+		carrier = gameObject.GetComponent<Carrier>();
+		rb = GetComponent<Rigidbody2D>();
+		aiState = GetComponent<Animator>();
+	}
+
+	private bool IsAIState(int hash) {
+		AnimatorStateInfo stateInfo = aiState.GetCurrentAnimatorStateInfo(0);
+		return (stateInfo.fullPathHash == hash);
+	}
+
+	void Update() {
+		if( !active )
+			return;
+
+		// Choose closest and most valuable item
+		Item wantedItem = gameMgr.GetMostValuableItem();
+		if( wantedItem == null )
+			return;
+
+		relativeItemPos = wantedItem.obj.transform.InverseTransformPoint(transform.position);
+
+		// State changes
+		// ---
+		if( relativeItemPos.magnitude <= carrier.reach ) {
+			aiState.SetTrigger(itemInRangeHash);
+		}
+
+		if( carrier.hasItem ) {
+			aiState.SetTrigger(hasItemHash);
+		}
+
+		// TODO: Scare
+
+		// State specific behaviour
+		// ---
+
+		if( IsAIState(pickingUpItemStateHash) ) {
+			// Pick up
+			carrier.PickUp(wantedItem.obj);
+		}
+	}
+
+	void FixedUpdate() {
+		// State specific behaviour
+		// ---
+		float moveHorizontal = 0f;
+
+		if( IsAIState(searchingStateHash) ) {
+			// Move towards item
+			moveHorizontal = (relativeItemPos.x > 0) ? -1 : 1;
+			print("Searching");
+		} else if( IsAIState(leavingStateHash) || IsAIState(fleeingStateHash) ) {
+			// TODO: Move to closest exit
+			moveHorizontal = true ? -1 : 1;
+		}
+
+		Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+
+		rb.AddForce(movement * speed);
+	}
+}
