32c63c3740962d6ae91e2514b3f6f9c961447d07
Stefan Grassberger <stefan.grassberger@gmail.com>
2017-12-05 00:31:03 +0100

    Add music

---
+++ Home Invasion/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public static SoundManager instance = null;
+
+	void Awake() {
+		if( instance == null )
+			instance = this;
+		else if( instance != null )
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+	}
+}
