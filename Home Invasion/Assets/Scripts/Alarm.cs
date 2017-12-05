09692df3a91f6fcc347db58602007ee03a22af6c
Stefan Grassberger <stefan.grassberger@gmail.com>
2017-12-05 02:51:07 +0100

    Unfinished alarm behaviour

---
+++ Home Invasion/Assets/Scripts/Alarm.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour {
+	private GameManager gameMgr;
+	private Rigidbody2D rb;
+	private Animator alarmState;
+
+	public float duration;
+	private float timer;
+
+	private int activeStateHash = Animator.StringToHash("Base Layer.Active");
+	private int inactiveStateHash = Animator.StringToHash("Base Layer.Inactive");
+	private int triggeredStateHash = Animator.StringToHash("Base Layer.Triggered");
+
+	private int thiefInRangeHash = Animator.StringToHash("thiefInRange");
+	private int turnOnHash = Animator.StringToHash("turnOn");
+	private int timeOutHash = Animator.StringToHash("timeOut");
+
+	void Start() {
+		gameMgr = GameManager.instance;
+		rb = GetComponent<Rigidbody2D>();
+		alarmState = GetComponent<Animator>();
+	}
+
+	private bool IsAlarmState(int hash) {
+		AnimatorStateInfo stateInfo = alarmState.GetCurrentAnimatorStateInfo(0);
+		return (stateInfo.fullPathHash == hash);
+	}
+
+	void Update() {
+		if( gameMgr.doingSetup || gameMgr.paused )
+			return;
+
+		// State changes
+		// ---
+
+		// State specific behaviour
+		// ---
+
+		if( IsAlarmState(triggeredStateHash) ) {
+			timer += Time.deltaTime;
+			if( timer >= duration ) {
+				timer = 0f;
+				alarmState.SetBool(timeOutHash, true);
+			}
+		}
+
+		if( IsAlarmState(inactiveStateHash) ) {
+			alarmState.SetBool(thiefInRangeHash, false);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		print("trigger" + other);
+		// Alarm gets triggered by nearby thieves
+		if( IsAlarmState(activeStateHash) && other.gameObject.tag == "Thief" ) {
+			alarmState.SetBool(thiefInRangeHash, true);
+		}
+	}
+}
